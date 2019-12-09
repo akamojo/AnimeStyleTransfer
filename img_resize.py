dbb1e4c25916ed8437afe0c01d1b4d6daa55d55c
jells123 <julka.bedziechowska@gmail.com>
2019-12-09 22:08:38 +0100

    Update img resize script

--- img_resize.py
+++ img_resize.py
@@ -1,15 +1,20 @@
 import PIL
 from PIL import Image
 import os
+import sys
 
-width, height = 32, 32 # TODO: move to cmd line arguments?
+width, height = int(sys.argv[1]), int(sys.argv[1])
+resized_dir = f"{width}x{height}"
+if not os.path.isdir(resized_dir):
+	os.mkdir(resized_dir)
+	
 files_list = os.listdir()
 for idx, filename in enumerate(files_list):
 	if filename.endswith('.jpg'):
 		try:
 			with Image.open(filename) as im:
 				im_resized = im.resize((width, height), PIL.Image.LANCZOS)
-				im_resized.save(os.path.join(f'{width}x{height}', filename), "JPEG")
+				im_resized.save(os.path.join(resized_dir, filename), "JPEG")
 				if idx % 25 == 0:
 					print(f"{idx} / {len(files_list)} processed")
 		except IOError:
